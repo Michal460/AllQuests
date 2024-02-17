74d91263dca364fe79baaf09662833eac87ce876
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-02-17 16:33:20 +0300

    Update main.cpp

--- StartLearning/main.cpp
+++ StartLearning/main.cpp
@@ -1,27 +1,7 @@
-#include "iostream"
-#include "string"
+#include <string>
 
-class Person{
-private:
+struct Tab{
     std::string name;
-    int age;
-public:
-    Person(std::string n, int a) : age(a), name(n) {};
-    void getInfo(){ std::cout << name << " " << age << " "; }
+    int salary;
 };
-
-class Student : public Person{
-private:
-    int studentID;
-public:
-    Student(std::string n, int a, int id) : Person(n,a), studentID(id) {};
-    void getInfo(){
-        Person::getInfo();
-        std::cout << studentID;
-    }
-};
-
-int main(){
-    Student st("Michael", 18, 1153);
-    st.getInfo();
-}
+struct Tlist
