bbda3e9878f771533747eff10578e4594acf7bbb
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-04-04 11:26:13 +0300

    Изменил данные в вводе и в выводе

--- StartLearningCPP/DataAll.h
+++ StartLearningCPP/DataAll.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include <iomanip>
 #include <algorithm>
+#include <Windows.h>
 
 class Personal_Data {
 private:
@@ -14,7 +15,7 @@ private:
 	int data_day, data_month, data_year, salary;
 public:
 	Personal_Data(std::string srn, std::string n, std::string scn, int dd, int dm, int dy, std::string jt, int s);
-	
+
 	void getSurname(std::string& srn);
 	void getName(std::string& n);
 	void getSecname(std::string& scn);
@@ -36,19 +37,19 @@ public:
 	~Personal_Data();
 };
 
-class EmpList{
+class EmpList {
 private:
-    std::vector<Personal_Data> data;
+	std::vector<Personal_Data> data;
 public:
-    void addData(Personal_Data Emp);
-    void outputConsole();
-    void inputFile();
-    void outputFile();
-    void inputConsole();
-    void deleteData(std::string name);
-    void sortAge();
-    static bool sortAgeDown(Personal_Data &p1, Personal_Data &p2);
-    void table();
+	void addData(Personal_Data Emp);
+	void outputConsole();
+	void inputFile(std::string);
+	void outputFile(std::string);
+	void inputConsole();
+	void deleteData(std::string name);
+	void sortAge();
+	static bool sortAgeDown(Personal_Data& p1, Personal_Data& p2);
+	void table();
 };
 
 #endif
