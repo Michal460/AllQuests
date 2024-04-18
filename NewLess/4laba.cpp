b180e00708a4cfee475e457004e739e5132d10ac
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-04-18 11:19:20 +0300

    Устранил ошибки с возвращаемыми методами и оператоми

--- NewLess/4laba.cpp
+++ NewLess/4laba.cpp
@@ -6,59 +6,59 @@ private:
     int* matrix;
 
 public:
-    TMyMatrix(int t, int c) : total(t), count(c) { matrix = new int[total*count]{0}; }
+    TMyMatrix(int t, int c) : total(t), count(c) { matrix = new int[total * count] {0}; }
     TMyMatrix(int* m, int t, int c) : TMyMatrix(t, c) { set_coords(m, total, count); }
     TMyMatrix(const TMyMatrix& other) : TMyMatrix(other.matrix, other.total, other.count) {}
 
-    void set_coords(int* m, int len, int con) { for(int i = 0; i < total*con; i++) matrix[i] = (i<len*con) ? m[i] : 0; }
+    void set_coords(int* m, int len, int con) { for (int i = 0; i < total * con; i++) matrix[i] = (i < len * con) ? m[i] : 0; }
 
     int* get_coords() { return matrix; }
     void totalM(int& t) { t = total; }
     void countM(int& c) { c = count; }
 
-    TMyMatrix operator+=(const TMyMatrix& other) { for(int i = 0; i < other.total*other.count; i++) matrix[i] += other.matrix[i]; }
-    TMyMatrix operator-=(const TMyMatrix& other) { for(int i = 0; i < other.total*other.count; i++) matrix[i] -= other.matrix[i]; }
-    TMyMatrix operator*=(const int& num) { for(int i = 0; i < total*count; i++) matrix[i] *= num; }
-    TMyMatrix operator/=(const int& num) { for(int i = 0; i < total*count; i++) matrix[i] /= num; }
-    bool operator==(const TMyMatrix& other) { for(int i = 0; i < other.total*other.count; i++) if(matrix[i] != other.matrix[i]) return 0; return 1; }
-    bool operator!=(const TMyMatrix& other) { for(int i = 0; i < other.total*other.count; i++) if(matrix[i] == other.matrix[i]) return 0; return 1; }
+    TMyMatrix operator+=(const TMyMatrix& other) { for (int i = 0; i < other.total * other.count; i++) matrix[i] += other.matrix[i]; }
+    TMyMatrix operator-=(const TMyMatrix& other) { for (int i = 0; i < other.total * other.count; i++) matrix[i] -= other.matrix[i]; }
+    TMyMatrix operator*=(const int& num) { for (int i = 0; i < total * count; i++) matrix[i] *= num; }
+    TMyMatrix operator/=(const int& num) { for (int i = 0; i < total * count; i++) matrix[i] /= num; }
+    bool operator==(const TMyMatrix& other) { for (int i = 0; i < other.total * other.count; i++) if (matrix[i] != other.matrix[i]) return 0; return 1; }
+    bool operator!=(const TMyMatrix& other) { for (int i = 0; i < other.total * other.count; i++) if (matrix[i] == other.matrix[i]) return 0; return 1; }
 
     TMyMatrix operator/(const int num);
     TMyMatrix operator+(const TMyMatrix& other);
     TMyMatrix operator-(const TMyMatrix& other);
     TMyMatrix operator*(const TMyMatrix& other);
 
-    ~TMyMatrix(){}
+    ~TMyMatrix() {}
 };
 
-TMyMatrix TMyMatrix::operator/(const int num){
-    if(num == 0) return;
-    for(int i = 0; i < total*count; i++) matrix[i] = matrix[i]/num;
+TMyMatrix TMyMatrix::operator/(const int num) {
+    if (num == 0) return TMyMatrix(this->total, this->count);
+    for (int i = 0; i < total * count; i++) matrix[i] = matrix[i] / num;
     return *this;
 }
 
-TMyMatrix TMyMatrix::operator+(const TMyMatrix& other){
-    if(other.total != total || other.count != count) return;
-    TMyMatrix res(total, count); 
-    for(int i = 0; i < other.total*other.count; i++) res.matrix[i] = matrix[i] + other.matrix[i];
+TMyMatrix TMyMatrix::operator+(const TMyMatrix& other) {
+    if (other.total != total || other.count != count) return TMyMatrix(this->total, this->count);
+    TMyMatrix res(total, count);
+    for (int i = 0; i < other.total * other.count; i++) res.matrix[i] = matrix[i] + other.matrix[i];
     return *this;
 }
 
-TMyMatrix TMyMatrix::operator-(const TMyMatrix& other){
-    if(other.total != total || other.count != count) return;
-    TMyMatrix res(total, count); 
-    for(int i = 0; i < other.total*other.count; i++) res.matrix[i] = matrix[i] - other.matrix[i];
+TMyMatrix TMyMatrix::operator-(const TMyMatrix& other) {
+    if (other.total != total || other.count != count) return TMyMatrix(this->total, this->count);
+    TMyMatrix res(total, count);
+    for (int i = 0; i < other.total * other.count; i++) res.matrix[i] = matrix[i] - other.matrix[i];
     return res;
 }
 
 TMyMatrix TMyMatrix::operator*(const TMyMatrix& other)
 {
-    if(other.total != count) return;
+    if (other.total != count) return TMyMatrix(this->total, this->count);
     TMyMatrix res(total, other.count);
-    for(int i = 0; i < total; i++){
-        for(int j = 0; j < other.count; j++){
-            for(int k = 0; k < other.total; k++){
-                res.matrix[total*i+j] += (matrix[total*i+k]*other.matrix[total*k+j]);
+    for (int i = 0; i < total; i++) {
+        for (int j = 0; j < other.count; j++) {
+            for (int k = 0; k < other.total; k++) {
+                res.matrix[total * i + j] += (matrix[total * i + k] * other.matrix[total * k + j]);
             }
         }
     }
@@ -67,8 +67,8 @@ TMyMatrix TMyMatrix::operator*(const TMyMatrix& other)
 
 int main(void)
 {
-    int ar1[] = {1, 2, 3, 6, 5, 4, 7, 8, 9};
-    int ar2[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int ar1[] = { 1, 2, 3, 6, 5, 4, 7, 8, 9 };
+    int ar2[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
     TMyMatrix* matrix1 = new TMyMatrix(ar1, 3, 3);
     TMyMatrix* matrix2 = new TMyMatrix(ar2, 3, 3);
@@ -78,7 +78,7 @@ int main(void)
 
     int* ares = res.get_coords();
 
-    for(int i = 0; i < 9; i++) std::cout << ares[i] << " ";
+    for (int i = 0; i < 9; i++) std::cout << ares[i] << " ";
 
     return 0;
 }
