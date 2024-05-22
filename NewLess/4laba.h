e8e2189c92c9ea09c14b9994a1e31392a6faafd3
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-05-23 00:44:23 +0300

    Update 4laba.h

--- NewLess/4laba.h
+++ NewLess/4laba.h
@@ -1,1 +1,36 @@
+#ifndef MATRIX_H
+#define MATRIX_H
 
+#include <iostream>
+
+using namespace std;
+
+class matrix{
+    int *arr = nullptr;
+    int i, j;
+
+public:
+    explicit matrix(int i = 1, int j = 1);
+    matrix(const matrix& obj);
+    matrix(matrix&& obj) noexcept;
+    ~matrix();
+
+    friend ostream& operator<<(ostream &stream, const matrix& obj);
+    friend istream& operator>>(istream &stream, matrix& obj);
+
+    string toString() const;
+    void fromString(const string& str);
+    void set_i_j(const int& i, const int& j);
+
+    matrix operator+(const matrix& obj);
+    matrix operator-(const matrix& obj);
+    matrix& operator=(matrix& obj);
+    matrix operator*(const matrix& obj);
+    matrix operator/(const int& num);
+    matrix& operator+=(const matrix& obj);
+    matrix& operator-=(const matrix& obj);
+    bool operator==(const matrix& obj);
+    bool operator!=(const matrix& obj);
+};
+
+#endif //MATRIX_H
