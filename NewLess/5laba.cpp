28baed279bd7195ce30ae240fd803e30541959c3
Michal460 <147305790+Michal460@users.noreply.github.com>
2024-05-23 11:02:28 +0300

    Update 5laba.cpp

--- NewLess/5laba.cpp
+++ NewLess/5laba.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 
-class Triad{
-    int x1, x2, x3;
-
-public:
-    void up_x1(){ x1++; }
-    void up_x2(){ x2++; }
-    void up_x3(){ x3++; }
-};
-
-class Time: public Triad{
+class Triad {
+protected:
     unsigned hours, minutes, seconds;
-
 public:
-    Time(int h, int m, int s) : hours(h), minutes(m), seconds(s) {}
-
-    void up_x1(){ hours == 23 ? hours = 0 : hours++; }
-    void up_x2(){ if(minutes == 59) { minutes = 0; up_x1(); } else minutes++; }
-    void up_x3(){ if(seconds == 59) { seconds = 0; up_x2(); } else seconds++; }
-
-    void set_time(unsigned int h, unsigned int m, unsigned int s){ hours = h; minutes = m; seconds = s; }
-    void get_time(unsigned int& h, unsigned int& m, unsigned int& s){ h = hours; m = minutes; s = seconds; }
-
-    ~Time(){}
+    Triad(int h, int m, int s) : hours(h), minutes(m), seconds(s) {}
+    virtual void up_x1() { hours++; }
+    virtual void up_x2() { minutes++; }
+    virtual void up_x3() { seconds++; }
 };
 
-int main(void){
+class Time : public Triad {
+
+public:
+    Time(int h, int m, int s) : Triad(h, m, s) {}
+
+    virtual void up_x1() override { hours == 23 ? hours = 0 : hours++; }
+    virtual void up_x2() override { if (minutes == 59) { minutes = 0; up_x1(); } else minutes++; }
+    virtual void up_x3() override { if (seconds == 59) { seconds = 0; up_x2(); } else seconds++; }
+
+    void set_time(unsigned int h, unsigned int m, unsigned int s) { hours = h; minutes = m; seconds = s; }
+    void get_time(unsigned int& h, unsigned int& m, unsigned int& s) { h = hours; m = minutes; s = seconds; }
+
+    ~Time() {}
+};
+
+int main(void) {
     unsigned hours, minutes, seconds;
     Time* t1 = new Time(1, 17, 59);
     t1->up_x3();
